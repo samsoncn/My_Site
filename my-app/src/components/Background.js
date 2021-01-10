b971790f8cc02b94ffa950a4c6056f39ed979553
samsonc <samsonc0514@gmail.com>
2021-01-10 02:59:21 -0500

    Added Auto-Text-Typing

--- my-app/src/components/Background.js
+++ my-app/src/components/Background.js
@@ -2,14 +2,33 @@ import React from 'react';
 import '../App.css';
 // import { Button } from './Button';
 import './Background.css';
+import Typical from 'react-typical';
 
 function Background() {
     return (
         <div className='bg-container'>
             <video src="/video/vid"></video>
-            {/* <image url="./public/img/bike.jpg"></image> */}
             <h1>Samson Chan</h1>
-            <h3><span class="txt-type"></span><span class="cursor typing">&nbsp;</span></h3> 
+            <h3><span class="txt-type"></span><span class="cursor typing">&nbsp;</span></h3>
+            <p> I'm a{' '}
+
+                <Typical
+                    loop={Infinity}
+                    wrapper='b'
+                    steps={[
+                        'Software Developer.', 
+                        1000, 
+                        'Web Developer.', 
+                        1000, 
+                        'Product Manager.',
+                        1000,
+                        'Badminton Coach.',
+                        1000,
+                        'Mountain Biker.',
+                        1000
+                    ]}
+                />
+            </p>
         </div>
     )
 }
