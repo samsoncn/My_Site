17ba82474a962831d78704a96de632dd743580ac
samsonc <samsonc0514@gmail.com>
2021-01-18 23:56:46 -0500

    Added Header for Galerry page

---
+++ my-app/src/components/Gallery/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Gallery.css';
+
+function Header() {
+    return (
+        <>
+        <div className='textBox'>
+            <h1>Welcome to the Gallery Universe.</h1>
+            <span>Feel Free to Share Your Favourite Memory.</span>
+        </div>
+        
+        </>
+    )
+
+}
+export default Header;
